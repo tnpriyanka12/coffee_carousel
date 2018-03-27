6648879d2a976b168f09ff49bbac4cf87b1cbafd
tnpriyanka12 <tnpriyanka12@gmail.com>
2018-03-27 22:27:22 +1100

    session control & user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,20 +55,12 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def user_params
+    params.require(:user).permit(:username, :full_name, :email, :description, :password, :password_confirmation)
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email, :description, :password_digest)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
+
   resources :drinks
   resources :users
 
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+  
   root to: 'drinks#index'
 end
